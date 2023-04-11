8577d52fc4143ab23ba26bb48a98885d6cc005f4
GeorgePavlej <georgepavlej@gmail.com>
2023-04-11 18:12:47 +0100

    Update models.py PEP8

--- user/models.py
+++ user/models.py
@@ -14,10 +14,7 @@ class UserManager(BaseUserManager):
     use_in_migrations = True
 
     def _create_user(
-            self,
-            email: str,
-            password: str,
-            **extra_fields: dict[str, Any]
+        self, email: str, password: str, **extra_fields: dict[str, Any]
     ) -> AbstractUser:
         """Create and save a User with the given email and password."""
         if not email:
@@ -29,10 +26,10 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(
-            self,
-            email: str,
-            password: Optional[str] = None,
-            **extra_fields: dict[str, bool]
+        self,
+        email: str,
+        password: Optional[str] = None,
+        **extra_fields: dict[str, bool]
     ) -> AbstractUser:
         """Create and save a regular User with the given email and password."""
         extra_fields.setdefault("is_staff", False)
@@ -40,10 +37,7 @@ class UserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(
-            self,
-            email: str,
-            password: str,
-            **extra_fields: dict[str, bool]
+        self, email: str, password: str, **extra_fields: dict[str, bool]
     ) -> AbstractUser:
         """Create and save a SuperUser with the given email and password."""
         extra_fields.setdefault("is_staff", True)
@@ -66,4 +60,7 @@ class User(AbstractUser):
 
     objects = UserManager()
 
-    borrowed_books = models.ManyToManyField("books.Book", related_name="borrowed_by")
+    borrowed_books = models.ManyToManyField(
+        "books.Book",
+        related_name="borrowed_by"
+    )
