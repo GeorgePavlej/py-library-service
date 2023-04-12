178270c57a495007cbd680b2a3ccdf19ec310918
GeorgePavlej <georgepavlej@gmail.com>
2023-04-12 19:35:36 +0100

    Add BorrowingCreateSerializer in serializers.py

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -3,22 +3,31 @@ from .models import Borrowing
 from books.models import Book
 
 
-class BookSerializer(serializers.ModelSerializer):
+class BorrowingBookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
 
 
 class BorrowingReadSerializer(serializers.ModelSerializer):
-    book = BookSerializer()
+    book = BorrowingBookSerializer(read_only=True)
 
     class Meta:
         model = Borrowing
         fields = (
             "id",
-            "user",
             "book",
             "borrow_date",
             "expected_return_date",
             "actual_return_date",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("book", "borrow_date", "expected_return_date")
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        return Borrowing.create_borrowing(user=user, **validated_data)
