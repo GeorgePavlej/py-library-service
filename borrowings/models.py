2430c94c7708d6dee1ade293c89c2b889db0c9a1
GeorgePavlej <georgepavlej@gmail.com>
2023-04-12 19:34:43 +0100

    Add create_borrowing & return_borrowing method to models.py

--- borrowings/models.py
+++ borrowings/models.py
@@ -19,5 +19,24 @@ class Borrowing(models.Model):
         ]
     )
 
+    @classmethod
+    def create_borrowing(cls, user, book, borrow_date, expected_return_date):
+        if book.inventory == 0:
+            raise ValueError("This book is out of stock.")
+        book.inventory -= 1
+        book.save()
+        borrowing = cls(user=user, book=book, borrow_date=borrow_date, expected_return_date=expected_return_date)
+        borrowing.save()
+        return borrowing
+
+    def return_borrowing(self):
+        if self.actual_return_date:
+            raise ValueError("Borrowing has already been returned.")
+        else:
+            self.actual_return_date = datetime.date.today()
+            self.book.inventory += 1
+            self.book.save()
+            self.save()
+
     def __str__(self) -> str:
         return f"{self.user} borrowed {self.book}"
