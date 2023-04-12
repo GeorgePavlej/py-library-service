77610b72ffe7802a10de869da3cc2640024af0d4
GeorgePavlej <georgepavlej@gmail.com>
2023-04-12 15:52:11 +0100

    Add Borrowing in models.py

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,23 @@
+import datetime
+
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    book = models.ForeignKey("books.Book", on_delete=models.CASCADE)
+    borrow_date = models.DateField(
+        validators=[MinValueValidator(datetime.date.today())]
+    )
+    expected_return_date = models.DateField(
+        validators=[MinValueValidator(datetime.date.today())]
+    )
+    actual_return_date = models.DateField(
+        null=True, blank=True, validators=[
+            MinValueValidator(datetime.date.today())
+        ]
+    )
+
+    def __str__(self) -> str:
+        return f"{self.user} borrowed {self.book}"
