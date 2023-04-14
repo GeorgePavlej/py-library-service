d60ec3606a41dc65b8a89cc463bb0778357e908d
GeorgePavlej <georgepavlej@gmail.com>
2023-04-14 15:01:57 +0100

    Change SECRET_KEY for the project

--- library_service/settings.py
+++ library_service/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)acv#^(n1ltr6r72!aags%+v44^gq@l-d4457j9d!n7-zv1=4w"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
