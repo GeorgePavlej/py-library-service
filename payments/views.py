1e47e50aac74c031a0844446bfffa6d4b80ec82f
GeorgePavlej <georgepavlej@gmail.com>
2023-04-15 19:30:38 +0100

    Add permission_classes in views.py

--- payments/views.py
+++ payments/views.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Payment
 from .serializers import PaymentSerializer
 from .permissions import IsAdminOrOwner
@@ -9,6 +9,7 @@ from .permissions import IsAdminOrOwner
 class PaymentList(generics.ListCreateAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self) -> Any:
         user = self.request.user
