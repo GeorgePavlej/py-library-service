5e4e6b6642e296b281c7b20d877278ffe6ad4d83
GeorgePavlej <georgepavlej@gmail.com>
2023-04-15 17:45:36 +0100

    Update has_object_permission function in permissions.py

--- payments/permissions.py
+++ payments/permissions.py
@@ -3,4 +3,4 @@ from rest_framework import permissions
 
 class IsAdminOrOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj) -> bool:
-        return obj.user == request.user or request.user.is_staff
+        return obj.borrowing.user == request.user or request.user.is_staff
