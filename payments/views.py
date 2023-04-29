0141912caeffc00cc82bf6e8420e9c0bd667520d
GeorgePavlej <georgepavlej@gmail.com>
2023-04-29 09:44:12 +0100

    Delete comments in views.py

--- payments/views.py
+++ payments/views.py
@@ -47,45 +47,6 @@ class PaymentViewSet(viewsets.ModelViewSet):
         return Payment.objects.filter(borrowing__user=user)
 
 
-# class PaymentList(generics.ListCreateAPIView):
-#     queryset = Payment.objects.all()
-#     serializer_class = PaymentSerializer
-#     permission_classes = (permissions.IsAuthenticated,)
-#
-#     def create(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         amount = serializer.validated_data["amount"]
-#
-#         if amount <= 0:
-#             return Response(
-#                 {"detail": "The total amount due cannot be zero or negative."},
-#                 status=status.HTTP_400_BAD_REQUEST,
-#             )
-#
-#         session = create_stripe_session(amount)
-#         serializer.validated_data["session_url"] = session.url
-#         serializer.validated_data["session_id"] = session.id
-#
-#         self.perform_create(serializer)
-#         headers = self.get_success_headers(serializer.data)
-#         return Response(
-#             serializer.data, status=status.HTTP_201_CREATED, headers=headers
-#         )
-#
-#     def get_queryset(self) -> Any:
-#         user = self.request.user
-#         if user.is_staff:
-#             return Payment.objects.all()
-#         return Payment.objects.filter(borrowing__user=user)
-#
-#
-# class PaymentDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Payment.objects.all()
-#     serializer_class = PaymentSerializer
-#     permission_classes = (IsAdminOrOwner,)
-
-
 def stripe_success(request):
     return HttpResponse("Payment successful. Thank you for your payment!")
 
