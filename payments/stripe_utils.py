e1eaa00b85f23c90a20d4ad63e105164df860023
GeorgePavlej <georgepavlej@gmail.com>
2023-05-01 20:35:30 +0100

    Add success_url & cancel_url in stripe_utils.py

--- payments/stripe_utils.py
+++ payments/stripe_utils.py
@@ -1,13 +1,14 @@
 import stripe
 from django.conf import settings
 from django.core.exceptions import ValidationError
+from django.urls import reverse
 
 from payments.models import Payment
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
-def create_stripe_session(amount):
+def create_stripe_session(amount, success_url, cancel_url):
     if amount <= 0:
         raise ValidationError("Total amount due should be greater than zero")
     session = stripe.checkout.Session.create(
@@ -25,18 +26,18 @@ def create_stripe_session(amount):
             }
         ],
         mode="payment",
-        success_url="http://127.0.0.1:8000/api/payments/success",
-        cancel_url="http://127.0.0.1:8000/api/payments/cancel",
+        success_url=success_url,
+        cancel_url=cancel_url,
     )
-
     return session
 
 
-def create_stripe_payment(borrowing):
+def create_stripe_payment(request, borrowing):
     total_price = borrowing.get_total_borrowing_price()
-
+    success_url = request.build_absolute_uri(reverse("payments:success"))
+    cancel_url = request.build_absolute_uri(reverse("payments:cancel"))
     try:
-        session = create_stripe_session(total_price)
+        session = create_stripe_session(total_price, success_url, cancel_url)
     except ValueError as e:
         raise ValidationError("Invalid total price: {}".format(e))
 
@@ -46,5 +47,5 @@ def create_stripe_payment(borrowing):
         session_id=session.id,
         amount=total_price,
     )
-
     return payment
+
