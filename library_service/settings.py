1b80cb367d5308c9e955dd1cbe267337b1911e7c
GeorgePavlej <georgepavlej@gmail.com>
2023-05-05 20:56:51 +0100

    Add testserver to ALLOWED_HOSTS in settings.py

--- library_service/settings.py
+++ library_service/settings.py
@@ -32,7 +32,7 @@ STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["testserver"]
 
 
 # Application definition
