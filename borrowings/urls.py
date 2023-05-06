ce5bf04b80aa6542e26f779d6bfbc169f67f23b1
GeorgePavlej <georgepavlej@gmail.com>
2023-05-06 20:42:25 +0100

    Add path("<int:pk>/return-borrowing/") to borrowing urls

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from borrowings.views import BorrowingViewSet
@@ -5,6 +6,13 @@ from borrowings.views import BorrowingViewSet
 app_name = "borrowings"
 
 router = DefaultRouter()
-router.register("", BorrowingViewSet)
+router.register("", BorrowingViewSet, basename="borrowing")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "<int:pk>/return-borrowing/",
+        BorrowingViewSet.as_view({"post": "return_borrowing"}),
+        name="return-borrowing",
+    ),
+]
