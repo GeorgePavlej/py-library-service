1c8b2c1864a84e6aa6bfd2eadd37add25e0951c7
GeorgePavlej <georgepavlej@gmail.com>
2023-05-16 13:51:32 +0100

    Update payment models.py

--- payments/models.py
+++ payments/models.py
@@ -27,8 +27,8 @@ class Payment(models.Model):
         related_name="payments",
     )
     updated_at = models.DateTimeField(auto_now=True)
-    session_url = models.URLField(blank=True, null=True)
-    session_id = models.CharField(max_length=100)
+    session_url = models.URLField(max_length=255, blank=True, null=True)
+    session_id = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self) -> str:
