98f0ac844c0ad2e1fcfd0c0532bb88777842cd41
GeorgePavlej <georgepavlej@gmail.com>
2023-05-17 13:01:53 +0100

    Add "127.0.0.1" & "localhost" to ALLOWED_HOSTS

--- library_service/settings.py
+++ library_service/settings.py
@@ -32,7 +32,7 @@ STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 
 
 # Application definition
