adf2eedbc38172d6e40496472ebb3315b82ce5e2
GeorgePavlej <georgepavlej@gmail.com>
2023-05-20 20:27:33 +0100

    Delete create_borrowing & return_borrowing methods from Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,17 +1,8 @@
 import datetime
 
 from django.core.validators import MinValueValidator
-from django.db import models, transaction
-from django.http import HttpRequest
-from django.urls import reverse
+from django.db import models
 from django.utils import timezone
-from rest_framework.exceptions import ValidationError
-
-from books.models import Book
-from library_service import settings
-from payments.models import Payment
-from payments.stripe_utils import create_stripe_payment, create_stripe_session
-from user.models import User
 
 
 class Borrowing(models.Model):
@@ -37,77 +28,6 @@ class Borrowing(models.Model):
                 self.expected_return_date - self.borrow_date
         ).days
         return daily_fee * borrowing_duration
-
-    @classmethod
-    def create_borrowing(
-            cls,
-            request: HttpRequest,
-            user: User,
-            book: Book,
-            borrow_date: str,
-            expected_return_date: str
-    ):
-        if book.inventory == 0:
-            raise ValidationError("This book is out of stock.")
-
-        borrowing = cls(
-            user=user,
-            book=book,
-            borrow_date=borrow_date,
-            expected_return_date=expected_return_date,
-        )
-
-        total_amount_due = borrowing.get_total_borrowing_price()
-
-        if total_amount_due <= 0:
-            raise ValidationError(
-                "Invalid total price: "
-                "Total amount due should be greater than zero"
-            )
-
-        with transaction.atomic():
-            borrowing.save()
-            payment = create_stripe_payment(request, borrowing)
-        return borrowing
-
-    def return_borrowing(
-            self, is_payment_paid: bool, request=None) -> Payment | None:
-        if self.actual_return_date:
-            raise ValueError("Borrowing has already been returned.")
-        elif not is_payment_paid:
-            raise ValueError("Payment is not completed.")
-        else:
-            self.actual_return_date = datetime.date.today()
-            overdue_days = (
-                    self.actual_return_date - self.expected_return_date
-            ).days
-            if overdue_days > 0:
-                fine_amount = overdue_days * self.book.daily_fee \
-                              * settings.FINE_MULTIPLIER
-                fine_payment = Payment.objects.create(
-                    borrowing=self,
-                    payment_type=Payment.PaymentType.FINE,
-                    amount=fine_amount,
-                    status=Payment.PaymentStatus.PENDING,
-                )
-                success_url = (
-                    request.build_absolute_uri(reverse("payments:success"))
-                    + "?session_id={CHECKOUT_SESSION_ID}"
-                )
-                cancel_url = request.build_absolute_uri(
-                    reverse("payments:cancel")
-                )
-                session = create_stripe_session(
-                    fine_amount, success_url, cancel_url
-                )
-                fine_payment.session_url = session.url
-                fine_payment.session_id = session.id
-                fine_payment.save()
-
-                return fine_payment
-            else:
-                self.save()
-                return None
 
     def __str__(self) -> str:
         return f"{self.user} borrowed {self.book}"
