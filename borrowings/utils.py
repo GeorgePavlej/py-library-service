e6bd77e84427b9d2ba043a5f71630a61b190ca98
GeorgePavlej <georgepavlej@gmail.com>
2023-05-20 20:29:32 +0100

    Add send_telegram_message into utils.py

--- borrowings/utils.py
+++ borrowings/utils.py
@@ -1,10 +1,19 @@
 from datetime import date
+import requests
+from requests import Response
 
 from django.conf import settings
 from django.db.models import Q
 
 from borrowings.models import Borrowing
-from borrowings.signals import send_telegram_message
+
+
+def send_telegram_message(chat_id: int, message: str) -> Response:
+    bot_token = settings.BOT_TOKEN
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    payload = {"chat_id": chat_id, "text": message}
+    response = requests.post(url, data=payload)
+    return response
 
 
 def check_overdue_borrowings() -> None:
