9659c0ab2a8415b2392fdc5778caa74d67075468
GeorgePavlej <georgepavlej@gmail.com>
2023-05-21 20:32:23 +0100

    Update session_url field in payment models.py

--- payments/models.py
+++ payments/models.py
@@ -27,7 +27,7 @@ class Payment(models.Model):
         related_name="payments",
     )
     updated_at = models.DateTimeField(auto_now=True)
-    session_url = models.URLField(max_length=255, blank=True, null=True)
+    session_url = models.URLField(max_length=360, blank=True, null=True)
     session_id = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
 
